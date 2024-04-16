fc236091a112169b92ded7be362382053d503269
ecthon <ecthon@gmail.com>
2024-04-15 21:51:05 -0400

    feature: create route post:links

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,44 @@
 import fastify from 'fastify'
-const url = new URL('http://localhost:3333')
+import { z } from 'zod'
+import { sql } from './lib/postgres'
+import postgres from 'postgres'
+
 const app = fastify()
+
+app.post('/links', async (request, reply) => {
+  const createLinkSchema = z.object({
+    code: z.string().min(3),
+    url: z.string().url(),
+  })
+
+  const { code, url } = createLinkSchema.parse(request.body)
+
+  try {
+    const result = await sql/*sql*/`
+    INSERT INTO short_links (code, original_url)
+    VALUES (${code}, ${url})
+    RETURNING id
+  `
+
+    const link = result[0]
+
+    return reply.status(201).send({ shortLinkId: link.id })
+  } catch (error) {
+    if (error instanceof postgres.PostgresError) {
+      if (error.code === '23505') {
+        return reply.status(400).send( { message: '❌ Duplicated code!'})
+      }
+    }
+
+    console.log(error)
+    return reply.status(500).send({ message: 'Internal error.' })
+  }
+})
+
+
 
 app.listen({
   port: 3333,
 }).then(() => {
   console.log(` ✅ HTTP server running`)
-  console.log(`${url}`)
 })
